ee61f69140c29d9f5791b508c43c41fd25355b38
Peter Karlsson <peter@softwolves.pp.se>
2003-06-25 20:15:01 +0000

    Korta ner texten.

--- action.c
+++ action.c
@@ -563,11 +563,15 @@ unsigned char license(void)
               "useful, but WITHOUT ANY WARRANTY; without even the implied "
               "warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR "
               "PURPOSE.  See the GNU General Public License for more "
-              "details.\n\n"
+              "details."
+#ifndef __C64__
+              "\n\n"
               "You should have received a copy of the GNU General Public "
               "License along with this program; if not, write to the Free "
               "Software Foundation, Inc., 59 Temple Place, Suite 330, "
-              "Boston, MA  02111-1307  USA");
+              "Boston, MA  02111-1307  USA"
+#endif
+              );
 
 	return GAME_CONTINUES;
 }
